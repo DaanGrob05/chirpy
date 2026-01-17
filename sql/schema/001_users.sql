d522dad9373227311eec08f7df71e03dbb22afbc
DaanGrob05 <d.grob05@gmail.com>
2026-01-17 17:33:54 +0100

    add users migration

---
+++ sql/schema/001_users.sql
@@ -0,0 +1,11 @@
+-- +goose Up
+CREATE TABLE users (
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid (),
+  created_at timestamp NOT NULL,
+  updated_at timestamp NOT NULL,
+  email text NOT NULL UNIQUE
+);
+
+-- +goose Down
+DROP TABLE users;
+
